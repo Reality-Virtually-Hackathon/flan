8b271b8fe847cc0da99810732dffd6ff855ae2e1
ndass6 <ND@USComputers.net>
2017-10-08 18:01:13 -0400

    LAST TRY

--- Android/Assets/GoogleARCore/HelloARExample/Scripts/HelloARController.cs
+++ Android/Assets/GoogleARCore/HelloARExample/Scripts/HelloARController.cs
@@ -136,69 +136,28 @@ namespace GoogleARCore.HelloAR
             TrackableHit hit;
             TrackableHitFlag raycastFilter = TrackableHitFlag.PlaneWithinBounds | TrackableHitFlag.PlaneWithinPolygon;
 
-            if (count == 1)
+            if (Session.Raycast(m_firstPersonCamera.ScreenPointToRay(touch.position), raycastFilter, out hit))
             {
-                if (Session.Raycast(m_firstPersonCamera.ScreenPointToRay(touch.position), raycastFilter, out hit))
-                {
-                    // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
-                    // world evolves.
-                    var anchor = Session.CreateAnchor(hit.Point, Quaternion.identity);
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+                // world evolves.
+                var anchor = Session.CreateAnchor(hit.Point, Quaternion.identity);
 
-                    // Intanstiate an Andy Android object as a child of the anchor; it's transform will now benefit
-                    // from the anchor's tracking.
-                    dish = Instantiate(dishPrefab, hit.Point, Quaternion.identity,
-                        anchor.transform);
-                    dish.name = "Dish";
+                // Intanstiate an Andy Android object as a child of the anchor; it's transform will now benefit
+                // from the anchor's tracking.
+                dish = Instantiate(dishPrefab, hit.Point, Quaternion.identity,
+                    anchor.transform);
+                dish.name = "Dish";
 
-                    // Andy should look at the camera but still be flush with the plane.
-                    //andyObject.transform.LookAt(m_firstPersonCamera.transform);
-                    //andyObject.transform.rotation = Quaternion.Euler(0.0f,
-                    //    andyObject.transform.rotation.eulerAngles.y, andyObject.transform.rotation.z);
+                // Andy should look at the camera but still be flush with the plane.
+                //andyObject.transform.LookAt(m_firstPersonCamera.transform);
+                //andyObject.transform.rotation = Quaternion.Euler(0.0f,
+                //    andyObject.transform.rotation.eulerAngles.y, andyObject.transform.rotation.z);
 
-                    // Use a plane attachment component to maintain Andy's y-offset from the plane
-                    // (occurs after anchor updates).
-                    dish.GetComponent<PlaneAttachment>().Attach(hit.Plane);
+                // Use a plane attachment component to maintain Andy's y-offset from the plane
+                // (occurs after anchor updates).
+                dish.GetComponent<PlaneAttachment>().Attach(hit.Plane);
 
-                    placed = true;
-                }
-                else
-                {
-                    dish = Instantiate(dishPrefab);
-                    dish.name = "Dish";
-                    dish.transform.position = m_firstPersonCamera.transform.position + m_firstPersonCamera.transform.forward;
-                }
-            }
-            else if (count == 2)
-            {
-                GameObject.Find("Dish/Menu Items").GetComponent<MenuItemsManager>().NextMenuItems();
-            }
-            else if (count == 3)
-            {
-                GameObject.Find("Dish/Menu Items/Small Bowl").AddComponent<AddItemToDish>();
-            }
-            else if (count == 4)
-            {
-                GameObject.Find("Dish/Menu Items").GetComponent<MenuItemsManager>().NextMenuItems();
-            }
-            else if (count == 5)
-            {
-                GameObject.Find("Dish/Menu Items/Brown Rice").AddComponent<AddItemToDish>();
-            }
-            else if (count == 6)
-            {
-                GameObject.Find("Dish/Menu Items").GetComponent<MenuItemsManager>().NextMenuItems();
-            }
-            else if (count == 7)
-            {
-                GameObject.Find("Dish/Menu Items/Beef").AddComponent<AddItemToDish>();
-            }
-            else if (count == 8)
-            {
-                GameObject.Find("Dish/Menu Items").GetComponent<MenuItemsManager>().NextMenuItems();
-            }
-            else if (count == 9)
-            {
-                GameObject.Find("Dish/Menu Items/Scallions").AddComponent<AddItemToDish>();
+                placed = true;
             }
         }
 
